5b44d68b58827c642f250f52685df00a5c751359
JoyMaveke <Mavekejoy@gmail.com>
2024-07-17 01:48:10 +0300

    changes2

--- index.js
+++ index.js
@@ -8,28 +8,7 @@ fetch("http://localhost:6500/films")
       title.textContent = film.title
       film_details.appendChild(title)
 
-      const img = document.createElement("img")
-      img.src = film.poster
-      film_details.appendChild(img)
 
-      const runtime =document.createElement("h4")
-      runtime.textContent = film.runtime
-      film_details.appendChild(runtime)
 
-      const capacity = document.createElement("h4")
-      capacity.textContent = film.capacity
-      film_details.appendChild(capacity)
-
-      const description = document.createElement("p")
-      description.textContent = film.description
-      film_details.appendChild(description)
-
-      const showtime = document.createElement("h4")
-      showtime.textContent = film.showtime
-      film_details.appendChild(showtime)
-
-      const tickets_sold =document.createElement("h4")
-      tickets_sold.textContent = film.tickets_sold
-      film_details.appendChild(tickets_sold)
 
   }))
